0b13b9d3ad75cf86f10438af6ec17094759e3e55
SpencerTWilson <50560983+SpencerTWilson@users.noreply.github.com>
2024-12-26 17:39:31 -0600

    push

--- Addons/Better Rich Text Labels/better_rich_text_label.gd
+++ Addons/Better Rich Text Labels/better_rich_text_label.gd
@@ -3,9 +3,18 @@ extends RichTextLabel
 
 ## matching the base size to the screen size to make text appear consistant on every monitor
 @export var keep_text_size : bool = true
+
+## Decides to use screen width or screen height to determine size
+@export var use_height : bool = false
+
+## percentage of screen to take up for font size
 @export var base_size : float = 2.5
 
 func _process(delta):
 	if keep_text_size:
-		var vw = get_viewport_rect().size.x / 100
-		add_theme_font_size_override("normal_font_size", int(base_size * vw))
+		var v 
+		if use_height:
+			v = get_viewport_rect().size.y / 100
+		else:
+			v = get_viewport_rect().size.x / 100
+		add_theme_font_size_override("normal_font_size", int(base_size * v))
