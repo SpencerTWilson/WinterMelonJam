1e5f0d22e9fd95748ad1bde6af88684d44ca02fd
Day-Fire <128267461+Day-Fire@users.noreply.github.com>
2024-12-27 11:11:38 -0700

    healthbar

--- Scenes/Game/Buildings/capital.gd
+++ Scenes/Game/Buildings/capital.gd
@@ -2,5 +2,14 @@ extends StaticBody2D
 
 @export var blue_team: bool = false
 
-@export var health_max: int = 100
-@onready var cur_health: int = health_max
+@export var health_max: float = 100
+@onready var cur_health: float = health_max
+
+@export var healthbar : Sprite2D
+
+func _ready():
+	healthbar.material = healthbar.material.duplicate()
+
+func _process(delta):
+	healthbar.material.set_shader_parameter("value", cur_health / health_max)
+	cur_health -= 10 * delta
