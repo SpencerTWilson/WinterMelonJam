d1955b1a1112cfe078169412a5d4e4d689bd324e
Day-Fire <128267461+Day-Fire@users.noreply.github.com>
2024-12-28 01:04:20 -0700

    final moola maybe

--- Scenes/Game/dolla.gd
+++ Scenes/Game/dolla.gd
@@ -2,9 +2,13 @@ extends TextureRect
 
 @export var particles : GPUParticles2D
 @export var anim : AnimationPlayer
+@export var jank : Node2D
 
 func collect():
-	anim.play("collect")
+	anim.play("Collect")
 
 func splash():
 	particles.restart()
+
+func _process(_delta):
+	position.y = jank.position.y
