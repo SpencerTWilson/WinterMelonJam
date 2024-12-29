75179e2182df20e54317197798b82e697e20b2fc
SpencerTWilson <50560983+SpencerTWilson@users.noreply.github.com>
2024-12-28 23:59:18 -0600

    he

--- Scenes/Game/button_sounds_3.gd
+++ Scenes/Game/button_sounds_3.gd
@@ -4,6 +4,5 @@ extends CanvasLayer
 func _on_settings_button_pressed() -> void:
 	AudioManager._play_clip(AudioManager.button_sound, "UI")
 
-
 func _on_restart_button_pressed() -> void:
 	AudioManager._play_clip(AudioManager.button_sound, "UI")
