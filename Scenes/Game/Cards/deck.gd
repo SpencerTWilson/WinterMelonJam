5c76c3ef6293faf483bac18c4c11d95407b94115
SpencerTWilson <50560983+SpencerTWilson@users.noreply.github.com>
2024-12-28 23:02:40 -0600

    here

--- Scenes/Game/Cards/deck.gd
+++ Scenes/Game/Cards/deck.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends Node2D
 
 @export var game_manager: GameManager
 @export var hand: CardSlot
--- Scenes/Game/Cards/vfx_manager.gd
+++ Scenes/Game/Cards/vfx_manager.gd
@@ -2,25 +2,28 @@ extends Node2D
 
 #Formatted like:
 #{"effect name": Node}
-@export var effects: Dictionary = {}
+@export var effects: Array[GPUParticles2D]
 #This will be full of the names of the VFX we want on
 var active_effects: Array[String] = []
 
 func _ready() -> void:
-	pass
-	#print(self)
-	#for effect in effects:
-		#effects[effect] = get_node(effects[effect])
-	#
-	#for effect in effects:
-		#if active_effects.has(effect):
-			#effects[effect].emitting = true
-		#else :
-			#effects[effect].emitting = false
+	_card_types_to_effects()
+	
+	for effect in effects:
+		if active_effects.has(effect.name.to_lower()):
+			effect.emitting = true
+		else :
+			effect.emitting = false
 
+func _card_types_to_effects():
+	for type in $"..".tags:
+		for effect in effects:
+			if effect.name.to_lower() == type:
+				active_effects.append(type)
+		
 #grabs the nodes so that the units can steal them hehe
 func get_active_effects():
 	var ret_effects: Array = []
 	for effect in active_effects:
-		ret_effects.append(effects[effect])
+		ret_effects.append(effect)
 	return ret_effects
