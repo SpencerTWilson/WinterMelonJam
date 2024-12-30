867934d971abe2804d11e34fb46fd774b314f754
Day-Fire <128267461+Day-Fire@users.noreply.github.com>
2024-12-29 23:59:16 -0700

    bob is deadonated

--- Scenes/Game/Abilities/bomb.gd
+++ Scenes/Game/Abilities/bomb.gd
@@ -6,7 +6,10 @@ extends Sprite2D
 @export var wiggle_speed: float = 0.3
 var wiggle_right: bool
 var drop_velocity: float = 0.1
+@onready var vfx = $"CanvasLayer/ColorRect"
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
+var radius = 0
 func _process(delta: float) -> void:
 	drop_velocity += drop_velocity/10
 	position.y += drop_velocity
@@ -17,12 +20,17 @@ func _process(delta: float) -> void:
 		position.x = lerpf(position.x,-wiggle_amplitude,wiggle_speed)
 	if is_equal_approx(wiggle_amplitude, position.x) or is_equal_approx(-wiggle_amplitude, position.x):
 		wiggle_right = !wiggle_right
+	
+	if texture == null:
+		radius += 1 * delta
+		vfx.material.set_shader_parameter("radius", radius)
 
 var done = false
 
 func _on_boom_timer_timeout() -> void:
 	if !done: AudioManager._play_clip(boom_sound,"SFX")
 	texture = null
+	animated_sprite_2d.visible = true
 	$BoomTimer.start()
 	if done: queue_free()
 	done = true
